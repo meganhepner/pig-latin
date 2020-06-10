a372d5f7d9a8f938d42a03cf5200b76834c060d5
Megan Hepner <megan.hepner@gmail.com>
2020-06-10 15:42:15 -0700

    separate functions into user and business logic
    Co-authored-by: Ruthie Newman ruthienewman@protonmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
   $("form#pigLatin").submit(function(event){
     event.preventDefault();
     let userInput = ($("input#word").val());
-      
-  });
+    const result = pig(userInput);
+    $("#result").text(result);
+  }); 
 });
+ 
 
-
-//function pigLatinGenerator()
-const vowel = ['a', 'e', 'i', 'o', 'u'];
-
-for (i=0; i<userInput.length; i++) {
-  if (userInput[i].indexOf === "a" || "e" || "i" || "o" || "u") {
-    const indexOfVowel = userInput.indexOf(vowel)
-    console.log(indexOfVowel);
-  } 
+function pig(userInput){
+  const searchTerm = 'A';
+  if (userInput.indexOf(searchTerm) === 0){
+    if(userInput.length === 1){
+  console.log("yay")
+  } else {
+console.log("nope")
+  
+// }
+};
