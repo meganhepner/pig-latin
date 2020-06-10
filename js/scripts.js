05e5c562b095239753a438f73a442c03b062ed00
Megan Hepner <megan.hepner@gmail.com>
2020-06-10 16:30:07 -0700

    work on pig latin for a while longer
     Co-authored-by: Ruthie Newman ruthienewman@protonmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,18 @@ $(document).ready(function() {
     $("#result").text(result);
   }); 
 });
- 
+
 
 function pig(userInput){
-  const searchTerm = 'A';
-  if (userInput.indexOf(searchTerm) === 0){
-    if(userInput.length === 1){
-  console.log("yay")
+  if (userInput.length === 1){
+    result = (userInput + "way");
+    $("#result").text(result);
   } else {
-console.log("nope")
-  
-// }
-};
+    //for (i=0; i<userInput.length; i++) {
+      if (userInput.indexOf === "a",  "e", "i",  "o",  "u") {
+      result = (userInput)
+        // userInput.concat(“ay”);
+      }
+    }
+  }
+}
